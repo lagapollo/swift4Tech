2415ed173034fd46d1014259a1d96dc1f0c3830e
Sylvain Laure <slaure.extern@groupeseb.com>
2017-10-05 08:54:16 +0200

    swift 4

---
+++ swift4Tech/swift4Tech/swift4.swift
@@ -0,0 +1,14 @@
+//
+//  swift4.swift
+//  swift4Tech
+//
+//  Created by Sylvain Laure on 05/10/2017.
+//  Copyright © 2017 Sylvain Laure. All rights reserved.
+//
+
+import Foundation
+
+
+public class swift4 {
+    
+}
